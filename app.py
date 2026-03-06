3b1bbb117a79d90ce6044dd9496c902e6dba37ad
Marvin <marvin.poppe@esders.de>
2026-03-06 09:21:43 +0100

    Füge Benutzer- und Login-Routen mit JSON-Antworten hinzu

--- app.py
+++ app.py
@@ -1,10 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello, World! \nThis is a simple Flask application running in a Docker container.'
+
+@app.route('/login', methods=['POST'])
+def login():
+    return jsonify({
+        "success": True,
+        "user": {
+            "name": "Max",
+            "istAdmin": True,
+            "istAbteilungsLeiter": True
+        }
+    })
 
 if __name__ == '__main__':
     app.run(debug=True)
